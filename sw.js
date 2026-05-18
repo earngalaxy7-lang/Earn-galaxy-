2134c8d058a710813f809c5e78ac847b8d274504
earngalaxy7-lang <earngalaxy7@gmail.com>
2026-05-18 13:35:59 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11022202
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+<meta name="monetag" content="0430d88830f318451182103afdd4f3b9">
